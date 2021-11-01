c4ea982991a34a10b31bacb72db0fb73b10b385b
Ronaldo Salmon <ronopedia@gmail.com>
2021-10-31 19:33:43 -0500

    Edited files

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#search").on("click",function(e){
+        e.preventDefault();
+        var text1= (document.getElementById("heroes").value);
+        $.ajax({
+            type:'GET',
+            url:'superheroes.php?query='+text1,
+            success:function(data){
+                $("#result").html('');
+                $("#result").html(data);
+            }
+        });
+    });
+});
